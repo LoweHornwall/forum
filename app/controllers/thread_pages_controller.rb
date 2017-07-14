5764aab0c0c70218acb461c578c61edf3e4d02ec
Lowe Hornwall <lowe@Lowes-MBP.home>
2017-07-14 19:27:17 +0200

    More css for thread_page show view.

--- app/controllers/thread_pages_controller.rb
+++ app/controllers/thread_pages_controller.rb
@@ -1,7 +1,8 @@
 class ThreadPagesController < ApplicationController
 	def show
 		@thread = ForumThread.find_by_name(params[:name])
-		if @page = @thread.thread_pages.find_by_page_num(params[:page_num])
+		@thread_pages = @thread.thread_pages
+		if @page = @thread_pages.find_by_page_num(params[:page_num])
 			@posts = @page.posts
 		else
 			#unless the page exists go gack to the first page
